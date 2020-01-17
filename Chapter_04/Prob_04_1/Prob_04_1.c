c497c474d28b9c2d3b0747e7d123734819460d7f
wooyoung <riverlike14@gmail.com>
2020-01-17 14:30:15 +0900

    Problem 4-1 solved

---
+++ Chapter_04/Prob_04_1/Prob_04_1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node {
+	int data;
+	struct _node * next;
+} Node;
+
+int main() {
+	Node * head = NULL;
+	Node * tail = NULL;
+	Node * cur = NULL;
+
+	Node * newNode = NULL;
+	int readData;
+
+	while (1) {
+		printf("Enter a number: ");
+		scanf("%d", &readData);
+		if (readData < 1)
+			break;
+
+		newNode = (Node*)malloc(sizeof(Node));
+		newNode->data = readData;
+		newNode->next = head;
+
+		if (tail == NULL)
+			tail = newNode;
+
+		head = newNode;
+	}
+	printf("\n");
+
+	printf("Print the whole data! \n");
+	if (head == NULL) 
+		printf("There are no saved numbers. \n");
+	else {
+		cur = head;
+		while (cur != NULL) {
+			printf("%d ", cur->data);
+			cur = cur->next;
+		}
+	}
+	printf("\n\n");
+
+	Node * delNextNode;
+	while (head != NULL) {
+		delNextNode = head->next;
+		free(head);
+		head = delNextNode;
+	}
+
+	return 0;
+}

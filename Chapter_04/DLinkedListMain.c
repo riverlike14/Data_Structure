b9dec5942c4d6e5c0983d1cb59be9f0b841f0cf1
wooyoung <riverlike14@gmail.com>
2020-01-21 11:36:12 +0900

    DLinkedList Main function Done

---
+++ Chapter_04/DLinkedListMain.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "DLinkedList.h"
+
+int main() {
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 11);
+	LInsert(&list, 11);
+	LInsert(&list, 22);
+	LInsert(&list, 22);
+	LInsert(&list, 33);
+
+	printf("Current number of data: %d\n", LCount(&list));
+
+	if (LFirst(&list, &data)) {
+		while (TRUE) {
+			printf("%d ", data);
+			if (!LNext(&list, &data))
+				break;
+		}
+	}
+	printf("\n\n");
+
+	if (LFirst(&list, &data)) {
+		while (TRUE) {
+			if (data == 22)
+				LRemove(&list);
+			
+			if (!LNext(&list, &data))
+				break;
+		}
+	}
+	printf("Currently remaining number of data: %d\n", LCount(&list));
+
+	if (LFirst(&list, &data)) {
+		while (TRUE) {
+			printf("%d ", data);
+			if (!LNext(&list, &data))
+				break;
+		}
+	}
+	printf("\n\n");
+
+	return 0;
+}

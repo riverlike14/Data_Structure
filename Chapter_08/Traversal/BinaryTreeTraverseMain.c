183f30865cf2c7489d61760414eae734382287b0
wooyoung <riverlike14@gmail.com>
2020-02-18 13:55:11 +0900

    Tree traverse complete

--- Chapter_08/Traversal/BinaryTreeTraverseMain.c
+++ Chapter_08/Traversal/BinaryTreeTraverseMain.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include "BinaryTree.h"
 
-void InorderTraverse(BTreeNode * bt) {
-	if (bt == NULL)
-		return;
-
-	InorderTraverse(bt->left);
-	printf("%d \n", bt->data);
-	InorderTraverse(bt->right);
-}
+void ShowSqaureData(int data);
 
 int main() {
 	BTreeNode * bt1 = MakeBTreeNode();
 	BTreeNode * bt2 = MakeBTreeNode();
 	BTreeNode * bt3 = MakeBTreeNode();
 	BTreeNode * bt4 = MakeBTreeNode();
+	BTreeNode * bt5 = MakeBTreeNode();
+	BTreeNode * bt6 = MakeBTreeNode();
 
 	SetData(bt1, 1);
 	SetData(bt2, 2);
 	SetData(bt3, 3);
 	SetData(bt4, 4);
+	SetData(bt5, 5);
+	SetData(bt6, 6);
 
 	MakeLeftSubTree(bt1, bt2);
 	MakeRightSubTree(bt1, bt3);
 	MakeLeftSubTree(bt2, bt4);
+	MakeRightSubTree(bt2, bt5);
+	MakeRightSubTree(bt3, bt6);
 
-	InorderTraverse(bt1);
+	PreorderTraverse(bt1, ShowSqaureData);
+	printf("\n");
+	InorderTraverse(bt1, ShowSqaureData);
+	printf("\n");
+	PostorderTraverse(bt1, ShowSqaureData);
+	printf("\n");
 
 	return 0;
+}
+
+void ShowSqaureData(int data) {
+	printf("%d ", data * data);
+
 }
